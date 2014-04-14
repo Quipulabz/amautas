b45a60afc3e1a6bbef59c205b096ba98690aeab8
Nick B. Palomino <nbpalomino@gmail.com>
2014-04-13 19:24:40 -0500

    Dando estilos generales

---
+++ app/views/layout/main.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>Amautas | Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    <link rel="stylesheet" href="{{ URL::asset('packages/Flat-UI/bootstrap/css/bootstrap.css') }}">
+    <!-- Loading Flat UI -->
+    <link rel="stylesheet" href="{{ URL::asset('packages/Flat-UI/css/flat-ui.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('packages/Flat-UI/css/demo.css') }}">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="{{ URL::asset('packages/Flat-UI/js/html5shiv.js') }}"></script>
+      <script src="{{ URL::asset('packages/Flat-UI/js/respond.min.js') }}"></script>
+    <![endif]-->
+</head>
+<body>
+    <header id="header">
+        @include('layout.navigation')
+    </header>
+    <div class="container">
+        @yield('content')
+    </div>
+    <footer id="footer">
+        @include('layout.footer')
+    </footer>
+    <script src="{{ URL::asset('packages/Flat-UI/js/jquery-1.10.2.min.js') }}"></script>
+    <script src="{{ URL::asset('packages/Flat-UI/js/jquery-ui-1.10.3.custom.min.js') }}"></script>
+    <script src="{{ URL::asset('packages/Flat-UI/js/jquery.ui.touch-punch.min.js') }}"></script>
+    <script src="{{ URL::asset('packages/Flat-UI/js/bootstrap.min.js') }}"></script>
+    <script src="{{ URL::asset('packages/Flat-UI/js/bootstrap-select.js') }}"></script>
+    <script src="{{ URL::asset('packages/Flat-UI/js/bootstrap-switch.js') }}"></script>
+    <script src="{{ URL::asset('packages/Flat-UI/js/flatui-checkbox.js') }}"></script>
+    <script src="{{ URL::asset('packages/Flat-UI/js/flatui-radio.js') }}"></script>
+    <script src="{{ URL::asset('packages/Flat-UI/js/jquery.placeholder.js') }}"></script>
+    <script src="{{ URL::asset('packages/Flat-UI/js/jquery.tagsinput.js') }}"></script>
+    <script src="{{ URL::asset('packages/Flat-UI/js/application.js') }}"></script>
+    <!-- // <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> -->
+    <!-- // <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script> -->
+    @yield('scripts')
+</body>
+</html>
---
+++ app/views/layout/navigation.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-inverse navbar-embossed navbar-lg" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
+            <span class="sr-only">Toggle navigation</span>
+        </button>
+        <a class="navbar-brand" href="#">Amautas</a>
+    </div>
+    <div class="collapse navbar-collapse" id="navbar-collapse-01">
+        <ul class="nav navbar-nav">           
+            <li class="active"><a href="#fakelink">Ofertas</a></li>
+            <li><a href="#fakelink">Recomendaciones<span class="navbar-unread"></span></a></li>
+            <li><a href="#fakelink">Cursos<span class="navbar-new">4</span></a></li>
+        </ul>           
+        <form class="navbar-form navbar-right" action="#" role="search">
+            <div class="form-group">
+                <div class="input-group">
+                    <input class="form-control" id="navbarInput-01" type="search" placeholder="Search">
+                    <span class="input-group-btn">
+                        <button type="submit" class="btn"><span class="fui-search"></span></button>
+                    </span>            
+                </div>
+            </div>               
+        </form>
+    </div><!-- /.navbar-collapse -->
+</nav><!-- /navbar -->
